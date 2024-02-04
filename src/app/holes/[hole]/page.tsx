73da6111ae3214d3bff0934ffdf534727e59992d
JesseCrans <jesse.crans@live.nl>
2024-02-04 15:13:18 +0100

    changed mobile responsivity

--- src/app/holes/[hole]/page.tsx
+++ src/app/holes/[hole]/page.tsx
@@ -33,16 +33,16 @@ const Hole = async ({
           />
         ))
       }
-      <div className = "flex justify-between items-center p-4">
+      <div className="flex justify-between items-center p-4">
         {
-          <Link className = {'text-2xl ' + (parseInt(params.hole) > 1 ? 'hover:opacity-60' : 'pointer-events-none opacity-40')} href={`/holes/${parseInt(params.hole) - 1}`}>
-            <FontAwesomeIcon icon = {faArrowLeft}/>
-          </Link> 
+          <Link className={'text-2xl ' + (parseInt(params.hole) > 1 ? 'hover:opacity-60' : 'pointer-events-none opacity-40')} href={`/holes/${parseInt(params.hole) - 1}`}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
         }
         {
-          <Link className = {'text-2xl ' + (parseInt(params.hole) < 18 ? 'hover:opacity-60' : 'pointer-events-none opacity-40')} href={`/holes/${parseInt(params.hole) + 1}`}>
-            <FontAwesomeIcon icon = {faArrowRight}/>
-          </Link> 
+          <Link className={'text-2xl ' + (parseInt(params.hole) < 18 ? 'hover:opacity-60' : 'pointer-events-none opacity-40')} href={`/holes/${parseInt(params.hole) + 1}`}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
         }
       </div>
     </PageLayout>
