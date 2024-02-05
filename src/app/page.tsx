bf8c6aa6aeb1547956f1b9837db2dd575d5163de
Hugo van Schalm <hugovanschalm@hotmail.com>
2024-02-05 18:57:31 +0100

    Added the latest winner to the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 import PageLayout from "./layouts/PageLayout";
 import Link from "next/link";
+import gameData from "../../public/game_data/game_database.json"
 
 export default function Home() {
+
+  const getLatestWinner = () => {
+    const latestGame = gameData[gameData.length - 1];
+
+    const bestScores = latestGame.scores.reduce((maxScores, currentScores) => {
+      const thisScore = currentScores.reduce((sum, currentVal) => sum + currentVal);
+      const bestScore = maxScores.reduce((sum, currentVal) => sum + currentVal);
+
+      return thisScore < bestScore ? currentScores : maxScores
+    })
+
+    return latestGame.players[latestGame.scores.indexOf(bestScores)]
+  };
+
   return (
     <PageLayout title="Home">
       <p>
         Welkom bij Pizza Golf Maandag!
       </p>
+      <br/>
+      <p>Winnaar van deze week: {getLatestWinner()}</p> 
       <br />
       <p>
         Op deze site kun je alles vinden over Pizza Golf Maandag. Strategieën voor elke hole, speler profielen en de statistieken van elke gespeelde ronde
